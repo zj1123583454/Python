b3e2ed8c2f5a264e22002ffd0c2c15959bcbd3ee
zj1123583454 <1123583454@qq.com>
2016-07-22 10:15:23 +0800

    Study Print

---
+++ print.py
@@ -0,0 +1,13 @@
+#!/usr/bin/evn python
+#-*-coding:utf-8-*-
+# Study Print
+# 2016 7.22
+# Bad Programmer
+print "Hello World!"
+print "I'm Bad Progarmmer"
+print "I like typeing this."
+print "This is fun"
+print "Yay! Printing."
+print "I'd much rather you 'not'."
+print 'I "said"do not not touch this'
+print "中文测试","你好 世界!" #显示中文需要采用utf-8编码方式
