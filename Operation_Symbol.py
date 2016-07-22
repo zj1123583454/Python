92ce5869ce1cd5127267724ac1beb95ec7c433be
zj1123583454 <1123583454@qq.com>
2016-07-22 10:47:35 +0800

    Study Operation Symbol

---
+++ Operation_Symbol.py
@@ -0,0 +1,16 @@
+#!/usr/bin/evn python
+print "I will now connt my chickens:"
+print "Hens",25+30/6         #30
+print "Roosters",100-25*3%4  #97
+print "Now I will count the eggs:"
+print 3+2+1-5+4%2-1/4+4      #5
+print "Is it ture that 3+2<5-7?"
+print 3+2<5-7 								#Fales
+print "What is 3+2?",3+2			#5
+print "What is 5-7?",5-7			#-2
+print "Oh, that's why it's False"
+print "How about some more"
+print "Is ir greater",5>-2    #Ture
+print "Is it greater or equal?",5 >=-2  #Ture
+print "Is it less or equal",5<=-2				#Flase
+print 25/3,25.0/3.0
