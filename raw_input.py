eb97c9ea89810d3a1a6fcea2366fa9d0bbfee10e
zj1123583454 <1123583454@qq.com>
2016-07-23 23:29:10 +0800

    Study raw_input().py

---
+++ raw_input.py
@@ -0,0 +1,11 @@
+#/usr/bin/evn python
+#-*-coding:utf-8-*-
+name=raw_input("Please enter your name:\n")
+Age=int(raw_input("Please enter your age:\n"))
+print "your name is :%s"%name
+print "your age is %d"%Age
+print "再过20年你的年龄是:%d"%(Age+20)
+float_i=float(raw_input("enter int:\n"))
+print float_i
+number=int(float(raw_input("enter float:\n"))) #int()不可以转浮点数，所以要int(float(raw_input())),可以达到浮点数转int
+print number
