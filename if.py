3d88163be00cf06650d32113b985bbf55d05a86a
zj1123583454 <1123583454@qq.com>
2016-07-26 16:51:34 +0800

    if语句

---
+++ if.py
@@ -0,0 +1,17 @@
+#!/usr/bin/evn python 
+people=20
+cats=30
+dogs=15
+if people <cats:
+	print "Too many  cats !The world is saved"
+if people >cats:
+	print "Not many cats the world is saced"
+if people >dogs:
+	print "The world is dry"
+dogs +=5
+if people >=dogs:
+	print "people are greater than or equal to dogs"
+if people <=dogs:
+	print "People are less than or equal to dogs"
+if people == dogs:
+	print "People are dogs"
